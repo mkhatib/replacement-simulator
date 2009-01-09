c669077c5c8d3d1b3497575fae058f2ba430e01d
mkhatib727 <mkhatib727@1f3e3e2c-de02-11dd-a550-c997509ed985>
2009-01-09 06:50:19 +0000

    Added calculatePageFaults function that takes 4 parameters (ReferenceStrigns, PageSize, MemorySize, Policy), fully working with LRU and FIFO, returns number of page Faults

--- ReplacementSimulator.java
+++ ReplacementSimulator.java
@@ -8,24 +8,28 @@ import java.util.*;
 public final class ReplacementSimulator {
     
 	public static final int UNIFORM_DIST = 0, GAUSSIAN_DIST = 1;
+	public static final int FIFO = 0, LRU = 1;
 	
-
+	private int pageSize = 1; // 1KB
 
 	// {{{ ReplacementSimulator constructor
     /**
      * 
      */
     public ReplacementSimulator() {
-        int[] refString1 = generateReferenceString(1000,1000,GAUSSIAN_DIST);
-		int sum=0;
-		for(int i=0; i<refString1.length ;i++){
-			System.out.println(refString1[i]);
-			sum += refString1[i];
-		}
-		System.out.println("AVERAGE: " + sum/refString1.length);
+        int[] refString1 = generateReferenceString(1000,1000,UNIFORM_DIST);
+		/*int sum=0;
+				for(int i=0; i<refString1.length ;i++){
+					System.out.println(refString1[i]);
+					sum += refString1[i];
+				}
+				System.out.println("AVERAGE: " + sum/refString1.length);*/
 		
-			
-			
+		// RefString, pageSize, Memory Size, Policy
+		int pageFaults = calculatePageFaults(refString1, 1, 256, LRU); // FIFO, 16KB
+		System.out.println("Num Of Faults: " + pageFaults);
+		
+		
     }
 	// }}}
 	
@@ -68,6 +72,56 @@ public final class ReplacementSimulator {
 		return referenceString;
 	}
 
+	/**
+	 * calculatePageFaults
+	 *
+	 * @param  
+	 * @return 
+	 */
+	public int calculatePageFaults(int[] refStrings, int pageSize, int memorySize, int policy ) {
+		int numOfPages = (int) (memorySize/pageSize);
+		System.out.println("NUM OF PAGES: " + numOfPages);
+		
+		ArrayList<Integer> queue = new ArrayList<Integer>();
+		int numOfFaults=0;
+		if(policy == FIFO){
+			int nextPageIndex=0;
+			for(int i=0; i<refStrings.length; i++){
+				System.out.println("QUEUE: " + queue);
+				System.out.println("ITEM: " + refStrings[i]);
+				if(queue.contains(refStrings[i])) continue;
+				if(queue.size() < numOfPages) queue.add(refStrings[i]);
+				else queue.set(nextPageIndex, refStrings[i]);
+				numOfFaults++;
+				nextPageIndex++; nextPageIndex %= numOfPages;
+			}
+		}
+		else if(policy == LRU){
+			// Implemented Using Stack Implementation Methodology
+			boolean flag = true;
+			for(int i=0; i<refStrings.length; i++){
+				flag = true;
+				System.out.println("QUEUE: " + queue);
+				System.out.println("ITEM: " + refStrings[i]);
+				int index=-1;
+				if((index = queue.indexOf(refStrings[i])) != -1) 
+				{
+					queue.remove(index);
+					flag = false;
+				}	
+				if(queue.size() < numOfPages) queue.add(refStrings[i]);
+				else {
+					queue.remove(0);
+					queue.add(refStrings[i]);
+				}
+				if(flag) numOfFaults++;
+			}
+		}
+		return numOfFaults;
+	}
+
+	
+	
 	
 	public static void main(String[] args) {
 		ReplacementSimulator rs = new ReplacementSimulator();
