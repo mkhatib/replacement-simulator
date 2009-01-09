e7e39ce5d91aa4277ffceaa0e5fb23c89d36ad4a
mkhatib727 <mkhatib727@1f3e3e2c-de02-11dd-a550-c997509ed985>
2009-01-09 04:04:07 +0000

    Adding ReplacementSimulator.java Class Skelton

---
+++ trunk/ReplacementSimulator.java
@@ -0,0 +1,18 @@
+
+/**
+ * <<Class summary>>
+ *
+ * @author Mohammad Khatib &lt;&gt;
+ * @version $Rev$
+ */
+public final class ReplacementSimulator {
+    
+	// {{{ ReplacementSimulator constructor
+    /**
+     * 
+     */
+    public ReplacementSimulator() {
+        
+    }
+	// }}}
+}
